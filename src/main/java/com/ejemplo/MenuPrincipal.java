56bd22b28221addc11ad36a949307b6f00b62c36
Lenovo <Lenovo@PoloReptos>
2025-10-27 16:07:57 -0300

    menu principal

--- src/main/java/com/ejemplo/MenuPrincipal.java
+++ src/main/java/com/ejemplo/MenuPrincipal.java
@@ -7,7 +7,7 @@ public class MenuPrincipal extends JFrame {
 
     public MenuPrincipal() {
         setTitle("Menú Principal");
-        setSize(400, 300);
+        setSize(700, 500);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
 
